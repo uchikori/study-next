5824d1d476af5739d8e56428cb0db6d87865db3d
uchikori <ucchii.wa@gmail.com>
2022-12-11 17:22:46 +0900

    💄 スタイルモジュールの分割

--- components/Headline.jsx
+++ components/Headline.jsx
@@ -1,9 +1,9 @@
-import styles from '../styles/Home.module.css'
+import styles from './Headline.module.css'
 
 export function Headline(props) {
   
   const {fileName, children} = props;
-  console.log(props);
+  
   return (
     <div className="Home__top">
         <h1 className={styles.title}>
